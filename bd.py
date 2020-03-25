3f5b01252810647a5b73a6234bf89f8c7485da86
googlewaitme <62594550+googlewaitme@users.noreply.github.com>
2020-03-25 14:29:45 +0500

    Add files via upload

--- bd.py
+++ bd.py
@@ -1,15 +1,35 @@
 """
 	Вся работа с БД будет тут
 """
+import sqlite3
 
 
 class BD:
-	def __init__(self, cursor, name_bd):
+	def __init__(self, name_bd):
 		""" инитиализация курсора и бд"""
-		self.cursor = cursor
-		self.__str__ = name_bd
+		self.conn = sqlite3.connect(name_bd)
+		self.cursor = self.conn.cursor()
 
-	def create_bd(self):
+	def create_tables(self):
 		""" Creating new base of date"""
-		print('TEST creating BD')
+		self.cursor.execute("""
+			CREATE TABLE details
+			(id, name_detail, type)
+			""")
+		self.cursor.execute("""
+						CREATE TABLE drons
+					(id, name_dron, cost)
+				""")
+		self.cursor.execute("""
+					CREATE TABLE dron_map
+					(id, name, detail, count_detail)
+				""")
+		self.conn.commit()
 		return True
+
+	def insert_in_tables(self, details_table, drons_table, dron_map):
+		print("inserting in data")
+		# Добавляет в таблицы данные
+
+
+	def
