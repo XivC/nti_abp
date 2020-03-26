5ab93d1474c8e4abc218d99d6d191e50968be964
googlewaitme <62594550+googlewaitme@users.noreply.github.com>
2020-03-26 09:38:10 +0500

    Add files via upload

--- bd.py
+++ bd.py
@@ -1,6 +1,3 @@
-"""
-	Вся работа с БД будет тут
-"""
 import sqlite3
 
 
@@ -29,19 +26,27 @@ class BD:
 		return True
 
 	def insert_in_tables(self, details_table=[], drons_table=[], dron_map=[]):
-		details_table = tuple(details_table)
-		drons_table = tuple(drons_table)
-		dron_map = tuple(dron_map)
-		details_table, list_of_traces1 = self.filter_details_table(details_table)
+		details_table, list_of_traces1 = self.filter.filter_details_table(details_table)
 		self.cursor.executemany("INSERT INTO details VALUES (?,?,?)", details_table)
 		self.conn.commit()
-		drons_table, list_of_traces2 = self.filter_details_table(drons_table)
+		
+		drons_table, list_of_traces2 = self.filter.filter_details_table(drons_table)
 		self.cursor.executemany("INSERT INTO drons VALUES (?,?,?)", drons_table)
 		self.conn.commit()
-		dron_map, list_of_traces3 = self.filter_details_table(dron_map)
+
+		dron_map, list_of_traces3 = self.filter.filter_details_table(dron_map)
 		self.cursor.executemany("INSERT INTO dron_map VALUES (?,?,?,?)", dron_map)
 		self.conn.commit()
+		
 		return list_of_traces1 + list_of_traces3 + list_of_traces2
+
+
+class Filter:
+
+	def is_digit(self, string):
+		if not str(string).isdigit():
+			return (True, "строка {} не записана в {} базу данных, так как содержится буква в числе")
+		return (False, "Все хорошо")
 
 	def filter_details_table(self, details_table):
 		"""
@@ -52,15 +57,15 @@ class BD:
 		"""
 		list_of_traces = []
 		index_line = 0
-		dron_table = []
+		filtered_detail_table = []
 		for detail in details_table:
-			flag, string = self.filter.is_digit(detail[0])
+			flag, string = self.is_digit(detail[0])
 			index_line += 1
 			if flag:
 				list_of_traces.append(string.format(index_line, "Таблице деталей"))
 				continue
-			dron_table.append(dron)
-		return (dron_table, list_of_traces)
+			filtered_detail_table.append(detail)
+		return (filtered_detail_table, list_of_traces)
 
 	def filter_drons_table(self, drons_table):
 		"""
@@ -71,10 +76,10 @@ class BD:
 		"""
 		list_of_traces = []
 		index_line = 0
-		drons_tabl = []
+		filtered_drons_table = []
 		for dron in drons_table:
-			flag1, string1 = self.filter.is_digit(dron[0])
-			flag2, string2 = self.filter.is_digit(dron[2])
+			flag1, string1 = self.is_digit(dron[0])
+			flag2, string2 = self.is_digit(dron[2])
 			index_line += 1
 			if flag1 or flag2:
 				if flag1:
@@ -82,8 +87,8 @@ class BD:
 				else:
 					list_of_traces.append(string2.format(index_line, "Таблице дронов"))
 				continue
-			drons_tabl.append(dron)
-		return (drons_tabl, list_of_traces)
+			filtered_drons_table.append(dron)
+		return (filtered_drons_table, list_of_traces)
 
 	def filter_dron_map(self, dron_map):
 		"""
@@ -94,10 +99,10 @@ class BD:
 		"""
 		list_of_traces = []
 		index_line = 0
-		dron_tabl = []
-		for dron in dron_map:
-			flag1, string1 = self.filter.is_digit(dron[1])
-			flag2, string2 = self.filter.is_digit(dron[3])
+		filtered_dron_map_table = []
+		for dron in dron_map:	
+			flag1, string1 = self.is_digit(dron[1])
+			flag2, string2 = self.is_digit(dron[3])
 			index_line += 1
 			if flag1 or flag2:
 				if flag1:
@@ -105,39 +110,33 @@ class BD:
 				else:
 					list_of_traces.append(string2.format(index_line, "Технологические карты"))
 				continue
-			dron_tabl.append(dron)
-		return (dron_map, list_of_traces)
+			filtered_dron_map_table.append(dron)
+		return (filtered_dron_map_table, list_of_traces)
 
 
-class Filter:
-
-	def is_digit(self, string):
-		if type(string) == type(1) or type(string) == type(1.0):
-			return (False, "Все хорошо")
-		if not string.isdigit():
-			return (True, "строка {} не записана в {} базу данных, так как содержится буква в числе")
-		return (False, "Все хорошо")
 
 
 def test_bd():
 	# Тестовые данные
 	details_table = [
-		(1, 'detail1', 'batter'),
-		(2, 'detail2', 'batter'),
-		(3, 'dateil3', 'other')
+		[1, 'detail1', 'batter'],
+		[2, 'detail2', 'batter'],
+		[3, 'dateil3', 'other']
 	]
 	drons_table = [
-		(1, 'dron1', 100),
-		(2, 'dron2', 300)
+		[1, 'dron1', 100],
+		[2, 'dron2', 300]
 	]
 	dron_map = [
-		(1, 'dron1', 'detail1', 23),
-		(1, 'dron1', 'detail2', 2),
-		(2, 'dron2', 'detail1', 1),
-		(2, 'dron2', 'detail2', 345)
+		[1, 'dron1', 'detail1', 23],
+		[1, 'dron1', 'detail2', 2],
+		[2, 'dron2', 'detail1', 1],
+		[2, 'dron2', 'detail2', 345]
 	]
 	bd = BD()
 	bd.create_tables()
 	bd.insert_in_tables(drons_table=drons_table,
 						dron_map=dron_map,
 						details_table=details_table)
+
+test_bd()
